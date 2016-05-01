be9d30d54c186fe70e64a11cb531c75723175302
magdawi <mwimmer.mmt-b2014@fh-salzburg.ac.at>
2016-05-01 20:35:10 +0200

    edit login

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 5000;
+const users = [];
 
 app.use(express.static('public'));
 
@@ -10,13 +11,29 @@ http.listen(port, function(){
   console.log(`webserver listening on *:${port}`);
 });
 
-
 io.on('connection', function(socket){
+  socket.emit('chat message', 'Please Log In');
   console.log('a user connected');
 
-  socket.on('chat message', function(msg){
-    console.log(`got message '${msg}', broadcasting to all`);
-    io.emit('chat message', msg);
+socket.on('login', function(user){
+	var exist = false;
+	for (var i = 0; i < users.length; i++) {
+		if(users[i].name == user) exist = true;
+	}
+	if(!exist) {
+		users.push({name: user, id: socket.id});
+		socket.emit('chat message', `Login successful! Welcome '${user}'!`);
+	}
+	else {
+		socket.emit('chat message', `Login denied! Name '${user}' is already taken! Try again!`);
+	}
+  });
+
+
+
+
+socket.on('chat message', function(msg){
+	io.emit('chat message', msg);
   });
 
   socket.on('disconnect', function(){
@@ -26,3 +43,6 @@ io.on('connection', function(socket){
 
 // export app so we can test it
 exports = module.exports = app;
+
+
+
